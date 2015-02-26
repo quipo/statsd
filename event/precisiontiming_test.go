8c351ba1edd2ec4dfb7797582d38fb02e6265a19
Lorenzo Alberton <lorenzo@Lorenzos-MacBook-Pro.local>
2015-02-26 04:25:19 +0000

    adding .count property to aggregated timing events

--- event/precisiontiming_test.go
+++ event/precisiontiming_test.go
@@ -13,7 +13,7 @@ func TestPrecisionTimingUpdate(t *testing.T) {
 	e1.Update(e2)
 	e1.Update(e3)
 
-	expected := []string{"test.avg:0.005000|ms", "test.min:0.003000|ms", "test.max:0.007000|ms"}
+	expected := []string{"test.count:3|a", "test.avg:0.005000|ms", "test.min:0.003000|ms", "test.max:0.007000|ms"}
 	actual := e1.Stats()
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("did not receive all metrics: Expected: %T %v, Actual: %T %v ", expected, expected, actual, actual)
